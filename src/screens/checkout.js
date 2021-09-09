5ff2dfda4b656feaae8419c1ab372a07d1ac959b
Antonio Da Silva <aantonios@indracompany.com>
2021-09-09 01:17:22 -0300

    Commit com funcionalidades principais

---
+++ src/screens/checkout.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import { ScrollView, ImageBackground, View, Text, TextInput } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+
+const Checkout = ({ navigation, route }) => {
+    const [image, setImage] = useState();
+    const [title, setTitle] = useState();
+    const [price, setPrice] = useState();
+    const [total, setTotal] = useState();
+    const [count, setCount] = useState(0);
+    const [descriptionLocal, setDescriptionLocal] = useState();
+
+    useEffect(() => {
+        if (route.params) {
+            setTitle(route.params.title);
+            setDescriptionLocal(route.params.description);
+            setImage(route.params.image)
+            setCount(route.params.count)
+            setPrice(route.params.price)
+            setTotal(route.params.count*route.params.price)
+        }
+    }, [route.params])
+    return (
+        <View style={{ width: '100%', flex: 1, backgroundColor: 'white',alignItems: 'center', justifyContent: 'center', }}>
+            <View style={{height: '100%', width: '100%', alignItems: 'center', justifyContent: 'center'}}>
+                <View style={{ height: '50%', width: '100%', backgroundColor: '#242526' }}>
+                    <ImageBackground style={{ height: '100%', width: '100%' }} resizeMode="center" source={{ uri: image }} />
+                </View>
+                <View style={{ height: '50%', width: '100%' }}>
+
+                    <View style={{marginLeft:10,marginTop:10, height: '50%', width: '100%', backgroundColor: 'white', alignItems: 'flex-start', justifyContent: 'center' }} >
+                        <Text style={{ width:'80%' ,height: 25}}>{title}</Text>
+                        <Text style={{ width:'80%',height: 25}}>Qnt: {count}</Text>
+                        <Text  style={{ width:'80%',height: 25}}>Price: $ {price}</Text>
+                        <Text  style={{ width:'80%',height: 25, marginBottom: 20}}>Total: $ {total}</Text>
+                        <TextInput style={{ width:'80%',height: 40, borderWidth: 1, padding: 10 }} placeholder={'Cupom'}/>
+                    </View>
+                    <View style={{ height: '50%', width: '100%', backgroundColor: 'white', alignItems: 'center', justifyContent: 'center' }} >
+                    <TouchableOpacity style={{ height: '55%', width: '100%', backgroundColor: 'gray',borderRadius: 5 }}>
+                        <Text style={{marginRight:20,marginLeft:20, padding: 10, fontSize: 20, textAlign: "center" }}>Comprar</Text>
+                    </TouchableOpacity>
+                    </View>
+                </View>
+
+            </View>
+        </View>
+
+
+    )
+
+
+}
+
+export default Checkout;
---
+++ src/screens/description.js
@@ -0,0 +1,56 @@
+import React, {useEffect, useState } from 'react';
+import { ScrollView, ImageBackground,View, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const Description = ({ navigation, route }) => {
+
+    const [image, setImage] = useState();
+    const [title, setTitle] = useState();
+    const [price, setPrice] = useState();
+    const [total, setTotal] = useState();
+    const [count, setCount] = useState(0);
+    const [descriptionLocal, setDescriptionLocal] = useState();
+    useEffect(() => {
+        if (route.params) {
+            setTitle(route.params.title);
+            setDescriptionLocal(route.params.description);
+            setImage(`${route.params.imagePath}.${route.params.imageExtension}`)
+            setPrice(route.params.price);
+            setTotal(route.params.total)
+        }
+    }, [route.params])
+
+    return (
+        <View style={{ flex: 1}}>
+            <View style={{ height: '10%', width: '100%', backgroundColor: 'white', alignItems: 'center', justifyContent: 'center' }} >
+                <Text style={{ padding: 10, fontSize: 20, textAlign: "center" }}>{title}</Text>
+            </View>
+            <View style={{ height: '40%', width: '100%', backgroundColor: '#242526' }}>
+                <ImageBackground style={{ height: '100%', width: '100%' }} resizeMode="center" source={{ uri: image }} />
+            </View>
+            <View style={{ height: '40%', width: '100%', backgroundColor: 'white', alignItems: 'center', justifyContent: 'center' }}>
+                <ScrollView style={{ flex: 1 }}>
+                    <Text style={{ padding: 10, fontSize: 16, textAlign: "left" }}>{descriptionLocal}</Text>
+                </ScrollView>
+            </View>
+            <View style={{ flexDirection: 'row', height: '10%', width: '100%', backgroundColor: 'white', alignItems: 'center', justifyContent: 'space-between' }} >
+                <View style={{ padding: 10, flexDirection: 'row', height: '100%', width: '40%', backgroundColor: 'white', alignItems: 'center', justifyContent: 'space-between' }} >
+                    <TouchableOpacity style={{ height: 40, width: 40, backgroundColor: '#242526', borderRadius: 80, justifyContent:'center', alignItems:'center'  }} onPress={() => { count > 0 && setCount(prevCount => prevCount - 1) }}>
+                        <Text style={{ fontSize: 25, textAlign: "center" }}>-</Text>
+                    </TouchableOpacity>
+                    <Text style={{ fontSize: 20 }}>{count}</Text>
+                    <TouchableOpacity style={{ height: 40, width: 40, backgroundColor: '#242526', borderRadius: 80, justifyContent:'center', alignItems:'center' }} onPress={() => setCount(prevCount => prevCount + 1)}>
+                        <Text style={{ fontSize: 25, textAlign: "center" }}>+</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={{ height: '80%', width: '40%', backgroundColor: 'blue',marginRight:10 }}>
+                    <TouchableOpacity style={{ height: '100%', width: '100%', backgroundColor: '#242526' }} onPress={() => navigation.navigate('Checkout', {title: title, description: descriptionLocal, image: image, count: count,price: price})}>
+                        <Text style={{ padding: 10, fontSize: 20, textAlign: "center" }}>Adicionar</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default Description;
